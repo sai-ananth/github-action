cc1545f0a0a77cf577db8700deea75eb2f1e6000
Sai Ananth Vishwanatha <saiananth.kmit@gmail.com>
2021-04-21 22:15:34 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+// Launch microservice server- main.go
+package main
+
+import (
+	"lab12/microservice"
+	"log"
+)
+
+func main() {
+	s := microservice.NewServer("", "8000")
+	log.Fatal(s.ListenAndServe())
+}
